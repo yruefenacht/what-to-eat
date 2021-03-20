6b66238334d10d234b4b6513868f74977cafbc63
Yannick Rüfenacht <yannick.ruefenacht@hotmail.com>
2021-03-20 18:28:23 +0100

    Added db connection to firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'what-to-eat';
+  menus: Observable<any[]>
+  constructor(private firestore: AngularFirestore) {
+    this.menus = firestore.collection('Menu').valueChanges();
+  }
 }
