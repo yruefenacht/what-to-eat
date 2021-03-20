9356ef56e816409debcd2a8c5b0d98a6b21d21cf
Yannick Rüfenacht <yannick.ruefenacht@hotmail.com>
2021-03-20 17:28:58 +0100

    Added example env confing

---
+++ src/environments/environment.example.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    production: false,
+    apiKey: "<key>",
+    authDomain: "<project>.firebaseapp.com",
+    projectId: "<projectId>",
+    storageBucket: "<project>.appspot.ch",
+    messagingSenderId: "<senderId>",
+    appId: "<addId>",
+    measurementId: "<measurementId>"
+  };
