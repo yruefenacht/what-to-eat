c7a053a2587f097ab99282df2b28424ddf18b6e5
Yannick Rüfenacht <yannick.ruefenacht@hotmail.com>
2021-03-23 18:42:39 +0100

    Fixed linters

--- src/app/services/menu.service.ts
+++ src/app/services/menu.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
-import { of } from 'rxjs';
 import { Observable } from 'rxjs';
 import { Menu } from '../models/menu.model';
 import { IMenuService } from './menu.service.interface';
@@ -13,7 +12,7 @@ export class MenuService implements IMenuService {
   constructor(private firestore: AngularFirestore) { }
 
   getMenus(): Observable<Menu[]> {
-    let ref: AngularFirestoreCollection<Menu> = this.firestore.collection('Menu');
+    const ref: AngularFirestoreCollection<Menu> = this.firestore.collection('Menu');
     return ref.valueChanges();
   }
 }
