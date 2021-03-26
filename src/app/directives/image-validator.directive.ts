1192607a52546c34ada066ae6ac68138763e088c
Yannick Rüfenacht <yannick.ruefenacht@hotmail.com>
2021-03-26 12:33:09 +0100

    Fixed fucking linter errors

--- src/app/directives/image-validator.directive.ts
+++ src/app/directives/image-validator.directive.ts
@@ -2,8 +2,10 @@ import {AbstractControl, ValidationErrors, ValidatorFn} from '@angular/forms';
 
 export function requireImageFormat(formats: string[]): ValidatorFn {
   return (control: AbstractControl): ValidationErrors | null => {
-    if (!control.value) return null;
+    if (!control.value) {
+      return null;
+    }
     const valid = formats.includes(control.value.type);
     return !valid ? { imageFormat: true } : null;
-  }
+  };
 }
