084a1bdc67ab96d6fee7d7f79fb4da563cc1b1f5
Yannick Rüfenacht <yannick.ruefenacht@hotmail.com>
2021-04-06 20:46:57 +0200

    Completed login page markup

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,4 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ActivatedRoute, Router } from '@angular/router';
 import { of } from 'rxjs';
 
@@ -14,6 +18,12 @@ describe('LoginComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ LoginComponent ],
+      imports: [
+        MatButtonModule,
+        MatInputModule,
+        ReactiveFormsModule,
+        BrowserAnimationsModule
+      ],
       providers: [
         { provide: AuthService, useClass: AuthServiceMock },
         { provide: Router, useClass: class { navigate = jasmine.createSpy('navigate'); } },
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
@@ -9,17 +10,42 @@ import { AuthService } from '../services/auth.service';
 })
 export class LoginComponent implements OnInit {
 
+  loginForm: FormGroup;
   redirectUrl: string;
 
   constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
     private route: ActivatedRoute,
     private router: Router,
-    private authService: AuthService) { }
+  ) { }
 
   ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [
+        Validators.required,
+        Validators.email,
+      ]],
+      password: ['', [
+        Validators.required
+      ]]
+    });
+
     this.route.queryParams.subscribe(params => {
       this.redirectUrl = params.redirectUrl;
     });
+  }
+
+  submitHandler(): void {
+    const { email, password } = this.loginForm.value;
+  }
+
+  get email(): AbstractControl {
+    return this.loginForm.get('email');
+  }
+
+  get password(): AbstractControl {
+    return this.loginForm.get('password');
   }
 
 }
